672033fcc9b669e1716a2ce5fdfaf296d6b3e1f0
SergioOlachea <solachea_23@alu.uabcs.mx>
2026-03-08 14:33:47 -0700

    Login

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
